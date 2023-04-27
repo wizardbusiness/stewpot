23cb46171fc0dafbf519af86f00d979691307919
gkime <gkime@practice.com>
2023-04-26 19:02:08 -0700

    added root route and router component.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import Root from '../src/views/Root.tsx';
+import '../src/views/Root.tsx';
+import RouterErrorPage from './components/RouterError.tsx';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <RouterErrorPage />,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
