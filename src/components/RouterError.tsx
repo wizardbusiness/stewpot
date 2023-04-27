1654ebf623e61b9aa97b0a57567ad256e99b7bb1
gkime <gkime@practice.com>
2023-04-26 18:57:22 -0700

    added router error page

---
+++ src/components/RouterError.tsx
@@ -0,0 +1,23 @@
+import { isRouteErrorResponse, useRouteError } from 'react-router-dom';
+
+export default function RouterErrorPage() {
+  // header nav
+  const error = useRouteError();
+  console.error(error);
+  return (
+    <div id='router-error-page'>
+      <h1> Oh no! </h1>
+      <p> Something happened </p>
+      <p>
+        <i>
+          {
+            isRouteErrorResponse(error) ? 
+            (
+              error.error?.message || error.statusText
+            ) : 'Unknown Error' 
+          }
+        </i>
+      </p>
+    </div>
+  )
+}
