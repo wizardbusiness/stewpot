7c8a5d049b88da981aae998d3bf94b0e89496426
gkime <gkime@practice.com>
2023-04-27 13:55:06 -0700

    added routes and mocked views for tabs in sidebar pt 2.

--- src/views/Favorites.tsx
+++ src/views/Favorites.tsx
@@ -1,9 +1,11 @@
 import React, {useState, useEffect} from 'react'
+import './Favorites.css';
 export default function Favorites() {
   // saved recipes
 
   return (
-    <div>
+    <div id='favorites'>
+      Favorites Here
     </div>
   )
 }
--- src/views/Pantry.tsx
+++ src/views/Pantry.tsx
@@ -1,10 +1,13 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect} from 'react';
+import { Outlet } from 'react-router-dom';
 
 export default function Pantry() {
   // saved ingredients
 
   return (
     <div>
+      Pantry
+      <Outlet />
     </div>
   )
 }
