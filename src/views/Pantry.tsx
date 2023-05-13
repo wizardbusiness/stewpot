cd71d95f11203e1ba4ea69881c6ae96cd40365dd
Gabriel <gkime91@gmail.com>
2023-05-12 21:57:42 -0700

    added basic pantry view mock using mui data grid.

--- src/views/Pantry.tsx
+++ src/views/Pantry.tsx
@@ -1,13 +1,24 @@
 import React, {useState, useEffect} from 'react';
 import { Outlet } from 'react-router-dom';
+import { Box, TextField, Stack, Button, Typography } from '@mui/material';
+import { DataGrid } from '@mui/x-data-grid';
+import { pantryRows, pantryColumns } from '../consts/viewMocks';
+
 
 export default function Pantry() {
   // saved ingredients
   
   return (  
-    <div>
-      Pantry
+    <Box sx={{padding: '2em'}}>
+      <Stack direction='row' spacing={1} >
+        <TextField label='...Search Ingredients'/>
+        <Button variant='contained' size='small' sx={{backgroundColor: '#90767a'}}>
+          <Typography >Add Ingredient +</Typography>
+        </Button>
+      </Stack>
+      <Box sx={{width: '100%', height: '2em'}}/>
+      <DataGrid rows={pantryRows} columns={pantryColumns} />
       <Outlet />
-    </div>
+    </Box>
   )
 }
