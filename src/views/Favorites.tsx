a6771543bf7386af1e123992ab2fa8798e3d029b
Gabriel <gkime91@gmail.com>
2023-05-12 21:58:07 -0700

    added basic favorites view mock using mui cards.

--- src/views/Favorites.tsx
+++ src/views/Favorites.tsx
@@ -1,11 +1,39 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect} from 'react';
+import {Box, Grid, Card, CardContent, CardActions, CardMedia, Button, Typography,} from '@mui/material';
+import { favorites } from '../consts/viewMocks';
 
 export default function Favorites() {
   // saved recipes
 
   return (
-    <div id='favorites'>
-      Favorites Here
-    </div>
+    <Box sx={{padding: '3em'}}>
+      <Grid container gap={5}>
+        {favorites.map((recipe, index) => (
+          <Grid key={recipe.name} item>
+            <Card 
+              variant='outlined'
+              sx={{
+                height: 250, 
+                width: 250, 
+                display: 'flex', 
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            
+            >
+              <CardMedia 
+                  sx={{minHeight: '100%', minWidth: 150, filter: 'blur(2px)'}}
+                  image={`../../static/images/mockImages/stock-img-${index + 1}.jpg`}
+                  title={recipe.name}
+              />
+              <CardContent>
+                {recipe.name}
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+    
   )
 }
