8ea52deeaedd186a5954de2a12c80b2be55c5272
Gabriel <gkime91@gmail.com>
2023-05-13 22:10:40 -0700

    added justify prop to component to allow customization  of horizontal alignment.

--- src/components/common/SearchBar.tsx
+++ src/components/common/SearchBar.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import {Stack, Button, TextField} from '@mui/material';
 
-const SearchBar = ({btnText, btnColor}) => {
+const SearchBar = ({justify, btnText, searchDefText, btnColor}) => {
   return (
-    <Stack direction='row' sx={{width:'100%'}}>
-        <TextField variant='outlined' sx={{width: '25%'}} label='...Search Favorite Recipes'/>
+    <Stack direction='row' justifyContent={justify} sx={{width:'100%'}}>
+        <TextField variant='outlined' sx={{width: '25%'}} label={searchDefText}/>
         <Button variant='contained' size='large' sx={{backgroundColor: btnColor}}>{btnText}</Button>
     </Stack>
   )
