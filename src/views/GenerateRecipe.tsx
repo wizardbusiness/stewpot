3c7832712c0aea441e6b236624cb4f94011e5740
Gabriel <gkime91@gmail.com>
2023-05-15 17:41:41 -0700

    added components to generate recipes view to visualize what the end result will look like.

---
+++ src/views/GenerateRecipe.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import {  Box, TextField, Button, Typography, } from '@mui/material';
+
+function GenerateRecipe() {
+  return (
+    <Box sx={{padding: '2em', height: '100%', display: 'flex', flexDirection: 'column'}}>
+      <Typography paddingBottom='0.5em' color='#616161' variant='h2'>Generate Recipe</Typography> 
+      <Box sx={{width: '100%'}}>
+        <TextField  
+          id='prompt-textarea'
+          multiline
+          rows={8}
+          label='Write Recipe Prompt'
+          // placeholder="...write your recipe prompt here"
+          sx={{width: 'inherit'}}
+        ></TextField>
+        <Button variant='contained' size='large'>Create Recipe</Button>
+      </Box>
+      <Box padding='2em'></Box>
+      <fieldset style={{
+        // height: '20vh',
+        flexGrow: 1,
+        border: '1px solid lightgrey',
+        borderRadius: '4px'
+
+      }}
+      >
+        <legend>Output</legend>
+      </fieldset>
+    </Box>
+  )
+}
+
+export default GenerateRecipe;
