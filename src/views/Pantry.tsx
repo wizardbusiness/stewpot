00a4b05fe87e5135e2233dd1da963a37bd0f7c4c
Gabriel <gkime91@gmail.com>
2023-05-15 17:39:18 -0700

    added header to view for increased legibility.

--- src/views/Pantry.tsx
+++ src/views/Pantry.tsx
@@ -12,12 +12,16 @@ export default function Pantry() {
   
   return (  
     <Box sx={{padding: '2em'}}>
-        <SearchBar 
-          justify='flex-start' 
-          btnText='+ Ingredient' 
-          searchDefText='Add Ingredient' 
-          btnColor='#f6af41'
-        />
+      <Box>
+        
+      </Box>
+      <Typography paddingBottom='0.5em' color='#616161' variant='h2'>Pantry</Typography>
+      <SearchBar 
+        justify='flex-start' 
+        btnText='+ Ingredient' 
+        searchDefText='Add Ingredient' 
+        btnColor='#f6af41'
+      />
       <Box sx={{width: '100%', height: '2em'}}/>
       <DataGrid rows={pantryRows} columns={pantryColumns} />
       <Outlet />
