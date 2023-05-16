d938cba9fa9619dbeb1eb2d7fb39254d3d7204a0
Gabriel <gkime91@gmail.com>
2023-05-16 15:13:33 -0700

    changed header to be 10% of page height.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,33 @@
-import React, {useState, useEffect} from 'react';
+import { Avatar, Box, Typography } from "@mui/material";
+import Icon from '@mdi/react';
+import { mdiPotSteam } from '@mdi/js'
+
+
+const headerStyles = {
+  wrapper: {
+    height: '10%',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: '#faaa63',
+    padding: '1em'
+  },
+  left: {
+    display: 'flex',
+    alignItems: 'flex-end',
+  }
+};
 
 export default function Header() {
   // header nav
 
   return (
-    <div>
-    </div>
+    <Box sx={headerStyles.wrapper}>
+      <Box sx={headerStyles.left}>
+        <Icon color='#fff' size={3} path={mdiPotSteam}></Icon>
+      </Box>
+        
+        <Avatar></Avatar>
+    </Box>
   )
-}
+};
