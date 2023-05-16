f8882d9fb97a15f8d7a9552891f97c8d6d3185ca
Gabriel <gkime91@gmail.com>
2023-05-16 15:13:10 -0700

    added styling to force html body and root elements to 100% height. wasn't happening with cssBaseline alone

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,36 @@
 import { useState } from 'react';
+import { Outlet } from 'react-router-dom';
+import { CssBaseline, Box, GlobalStyles  } from '@mui/material';
+import Header from './components/Header';
+import Sidebar from './Sidebar';
 import LoginField from './components/LoginField';
 
-import './App.css'
-
 function App() {
-  
+  const [ loggedIn, setLoggedIn ] = useState(true);
   return (
     <>
-      <div className='login'>
-        <LoginField inputLabel=' Username' inputType='username'></LoginField>
-        <LoginField inputLabel=' Password' inputType='password'></LoginField>
-      </div>
+        <CssBaseline />
+        <GlobalStyles styles={{ html: {height: '100%'}, body: {height: '100%'}, '#root': {height: '100%'} }} />
+        {loggedIn ? 
+          <Box sx={{
+            display: 'flex',
+            minHeight: '100%'
+            
+          }}>
+            <Sidebar />
+            <Box sx={{
+              flexGrow: 1,
+            }}>
+              <Header />
+              <Outlet />
+            </Box>
+          </Box> 
+          : 
+          <>
+            <LoginField inputLabel=' Username' inputType='username'></LoginField>
+            <LoginField inputLabel=' Password' inputType='password'></LoginField>
+          </>
+        }
     </>
   )
 }
