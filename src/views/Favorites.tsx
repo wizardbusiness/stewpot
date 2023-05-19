8711ac2f7b12fe73ac368cebd44626b3e32970b9
Gabriel <gkime91@gmail.com>
2023-05-18 23:10:23 -0700

    removed old searchbar component.

--- src/views/Favorites.tsx
+++ src/views/Favorites.tsx
@@ -1,6 +1,5 @@
 import React, {useState, useEffect} from 'react';
 import {Box, Grid, Typography} from '@mui/material';
-import IngredientSearchbar from '../components/common/IngredientSearchbar';
 import RecipeCard from '../components/common/RecipeCard';
 import { favorites } from '../consts/dummyData';
 
@@ -10,12 +9,6 @@ export default function Favorites() {
   return (
     <Box sx={{padding: '2em'}}>
       <Typography paddingBottom='0.5em' color='#616161' variant='h2'>Favorites</Typography>
-      <IngredientSearchbar 
-        justify='flex-start' 
-        btnText='Go' 
-        searchDefText='Search Favorite Recipes' 
-        btnColor
-      />
       <Box sx={{width: '100%', height: '2em'}}/>
       <Grid container gap={5}>
         {favorites.map(recipe => (
--- src/views/Pantry.tsx
+++ src/views/Pantry.tsx
@@ -3,25 +3,16 @@ import { Outlet } from 'react-router-dom';
 import { Box, TextField, Stack, Button, Typography } from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid';
 import { pantryRows, pantryColumns } from '../consts/dummyData';
-import IngredientSearchbar from '../components/common/IngredientSearchbar';
 import { Search } from '@mui/icons-material';
-
 
 export default function Pantry() {
   // saved ingredients
-  
   return (  
     <Box sx={{padding: '2em'}}>
       <Box>
-        
       </Box>
       <Typography paddingBottom='0.5em' color='#616161' variant='h2'>Pantry</Typography>
-      <IngredientSearchbar 
-        justify='flex-start' 
-        btnText='+ Ingredient' 
-        searchDefText='Add Ingredient' 
-        btnColor='#f6af41'
-      />
+      <TextField variant='outlined' placeholder='Search Ingredients'/>
       <Box sx={{width: '100%', height: '2em'}}/>
       <DataGrid rows={pantryRows} columns={pantryColumns} />
       <Outlet />
