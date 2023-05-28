40b11b74cd619814b7d2fb4b80352f22540ab8c8
Gabriel <gkime91@gmail.com>
2023-05-28 00:28:25 -0700

    added placeholder login text and menu icon in prep for having settings. swapped around logo ordering for better flow

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,33 +1,52 @@
-import { Avatar, Box, Typography } from "@mui/material";
+import { Avatar, Box, AppBar, Typography, Toolbar, Stack, IconButton } from "@mui/material";
+import MenuIcon from '@mui/icons-material/Menu';
 import Icon from '@mdi/react';
-import { mdiPotSteam } from '@mdi/js'
+import { mdiPotSteam } from '@mdi/js';
+import { drawerWidth } from "../consts/dummyData";
 
 
 const headerStyles = {
   wrapper: {
-    height: '10%',
+    height: '100%',
     display: 'flex',
     justifyContent: 'space-between',
     alignItems: 'center',
     backgroundColor: '#faaa63',
-    padding: '1em'
   },
   left: {
     display: 'flex',
     alignItems: 'flex-end',
+  },
+  '& .MuiToolBar': {
+    height: '10%'
   }
 };
 
 export default function Header() {
-  // header nav
 
   return (
-    <Box sx={headerStyles.wrapper}>
-      <Box sx={headerStyles.left}>
-        <Icon color='#fff' size={3} path={mdiPotSteam}></Icon>
-      </Box>
-        
-        <Avatar></Avatar>
+    <Box sx={{display: 'flex', justifyContent: 'flex-start'}}>
+      <AppBar sx={{ height: '10%', width: `calc(100% - ${drawerWidth}px)`}}>
+        <Toolbar sx={headerStyles.wrapper}>
+          <Stack direction='row'>        
+          <Typography fontWeight={300} lineHeight='2em' fontSize='3em'>StewPot</Typography>
+          <Icon color='#FFF' size={3.5} path={mdiPotSteam}/>           
+          </Stack>
+            <Box display='flex' flexDirection='column'>
+              <Typography>Hi, Gabriel!</Typography>
+            
+            <Stack direction='row' alignItems='center'> 
+              <Avatar />
+              <IconButton>
+              <MenuIcon fontSize='large' sx={{color: 'white'}} />
+            </IconButton>
+            </Stack>
+            </Box>
+        </Toolbar>
+      </AppBar>
+        <Toolbar sx={{height: '100px'}}/>
     </Box>
+      
+      
   )
-};
+}
