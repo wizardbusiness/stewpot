30b1dcd4ef1ba59d27ebcc2c5e596f61ab1e35c2
Gabriel <gkime91@gmail.com>
2023-06-09 14:06:29 -0700

    added searchbar for recipe results.

--- src/views/FindRecipes.tsx
+++ src/views/FindRecipes.tsx
@@ -5,41 +5,51 @@ import {
   Typography,
 } from '@mui/material';
 import { ExpandMore } from '@mui/icons-material';
-import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import { GridColDef, GridRowsProp } from '@mui/x-data-grid';
 import AddIngredientModal from '../components/common/AddIngredientModal';
 import RecipeCard from '../components/common/RecipeCard';
-import { pantryRows, pantryColumns, favorites } from '../consts/dummyData'; // dummy data
+import SearchBar from '../components/common/SearchBar';
+import { pantryRows, pantryColumns, commonIngredientRows, favorites, favoritesInterface } from '../consts/dummyData'; // dummy data
 
-export default function FindRecipes({storedIngredients}) {
-  const [ selectedColumns, setSelectedColumns ] = useState<GridColDef[] | []>([]);
-  const [ selectedIngredients, setSelectedIngredients] = useState<object[] | []> ([]);
+export default function FindRecipes() {
+  const [ selectedColumns, setSelectedColumns ] = useState<GridColDef[]>([]);
+  const [ selectedIngredients, setSelectedIngredients] = useState<GridRowsProp> ([]);
+  const [ recipes, setRecipes ] = useState<favoritesInterface[]>(favorites);
+  const [ searchedRecipes, setSearchedRecipes ] = useState<favoritesInterface[]>([]);
 
   useEffect(() => {
     // show only relevant ingredient info in data grid
-    const relevantIngredientInfo = pantryRows.map((ingInfo) => {
+    const relevantIngredientInfo: GridRowsProp = pantryRows.map((ingInfo) => {
       return ({
         id: ingInfo.id,
-        ingredient: ingInfo.ingredient,
+        name: ingInfo.ingredient,
         qt: ''
       })
-    })
-    const relevantCols: GridColDef[] = pantryColumns.filter(cols => cols.field === 'ingredient' || cols.field === 'qt')
+    });
 
-     setSelectedIngredients(relevantIngredientInfo);
-     setSelectedColumns(relevantCols);
+    const relevantCols: GridColDef[] = pantryColumns.filter(cols => cols.field === 'ingredient' || cols.field === 'qt');
+    setSelectedIngredients(relevantIngredientInfo);
+    setSelectedColumns(relevantCols);
+    
   }, []);
+
+  useEffect(() => {
+    setSearchedRecipes(recipes)
+  }, [recipes]);
 
   // find recipe view
   return (
     <Box sx={{padding: '2em'}}>
       <Typography paddingBottom='0.5em' color='#616161' variant='h2'>Find Recipes</Typography>
+
       <AddIngredientModal infoText='Get Started &gt;' pantry={false} btnText='+ Add Ingredients From Pantry' searchTxt='Search Pantry' includeControls/>
       
-      <Box sx={{padding: '2em'}}>
+      <Box sx={{padding: '1em'}}>
+        <SearchBar placeholderText='Search Recipe Results' data={recipes} dataProperty='name' setResults={setSearchedRecipes} />
       </Box>
       <Grid container gap={5}>
-      {favorites.map(recipe => (
-        <RecipeCard key={recipe.name} name={recipe.name} id={recipe.id} starred={false}/>
+      {searchedRecipes.map(recipe => (
+        <RecipeCard key={recipe.id} name={recipe.name} id={recipe.id} starred={false}/>
       ))}
       </Grid>
     </Box>
