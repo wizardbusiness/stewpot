70e2e5c3c49430312639d173a10364c5971d325e
Gabriel <gkime91@gmail.com>
2023-06-09 13:53:37 -0700

    added styling to allow for scrolling of accordion, without overflowing div.

--- src/components/common/IngredientsAccordion.tsx
+++ src/components/common/IngredientsAccordion.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { 
   Accordion, 
   AccordionSummary, 
@@ -8,22 +9,23 @@ import { ExpandMore } from '@mui/icons-material';
 import { DataGrid, useGridApiRef, GridRowsProp } from '@mui/x-data-grid';
 
 const IngredientsAccordion = ({checked, selectedIngRows, columns, toggleAddRemoveRow}) => {
+
   return (
-    <Accordion>
-            <AccordionSummary
-              expandIcon={<ExpandMore />}
-              aria-controls='panel1a-content'
-              id='panel1a-header'
-            >
-              <Typography>Selected Ingredients</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              {checked.length > 0 ? 
-                <DataGrid columns={columns} rows={selectedIngRows} hideFooter sx={{maxHeight: '25%'}}/> :
-                <Typography sx={{display: 'flex', justifyContent:'center', color: "#acacac"}}>No Ingredients Selected</Typography>
-              }
-            </AccordionDetails>
-          </Accordion>
+    <Accordion sx={{maxHeight: '100%'}}>
+      <AccordionSummary
+        expandIcon={<ExpandMore />}
+        aria-controls='panel1a-content'
+        id='panel1a-header'
+      >
+        <Typography>Selected Ingredients</Typography>
+      </AccordionSummary>
+      <AccordionDetails sx={{maxHeight: 225, overflow: 'scroll'}}>
+        {checked.length > 0 ? 
+          <DataGrid columns={columns} rows={selectedIngRows} hideFooter sx={{maxHeight: '10%'}}/> :
+          <Typography sx={{display: 'flex', justifyContent:'center', color: "#acacac"}}>No Ingredients Selected</Typography>
+        }
+      </AccordionDetails>
+    </Accordion>
   )
 }
 
