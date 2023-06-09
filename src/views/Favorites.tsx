b57cb776190abcb2721a6d7ff09eb9daacb127e0
Gabriel <gkime91@gmail.com>
2023-06-09 13:54:57 -0700

    added search logic to favorites.

--- src/views/Favorites.tsx
+++ src/views/Favorites.tsx
@@ -1,18 +1,27 @@
 import React, {useState, useEffect} from 'react';
 import {Box, Grid, Typography} from '@mui/material';
 import RecipeCard from '../components/common/RecipeCard';
-import { favorites } from '../consts/dummyData';
+import SearchBar from '../components/common/SearchBar';
+import { favorites, favoritesInterface } from '../consts/dummyData';
 
 export default function Favorites() {
   // saved recipes
 
+  const [recipes, setRecipes ] = useState<favoritesInterface[]>(favorites);
+  const [ searchedRecipes, setSearchedRecipes ] = useState<favoritesInterface[]>([]);
+
+  useEffect(() => {
+    setSearchedRecipes(recipes);
+  }, [recipes])
+
   return (
     <Box sx={{padding: '2em'}}>
       <Typography paddingBottom='0.5em' color='#616161' variant='h2'>Favorites</Typography>
+      <SearchBar placeholderText='Search Recipes' data={recipes} dataProperty='name' setResults={setSearchedRecipes} />
       <Box sx={{width: '100%', height: '2em'}}/>
       <Grid container gap={5}>
-        {favorites.map(recipe => (
-          <RecipeCard name={recipe.name} id={recipe.id} starred/>
+        {searchedRecipes.map(recipe => (
+          <RecipeCard key={recipe.id} name={recipe.name} id={recipe.id} starred/>
         ))}
       </Grid>
     </Box>
