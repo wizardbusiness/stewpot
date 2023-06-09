6cf2584181805536355798c6bcaa4bba24469efd
Gabriel <gkime91@gmail.com>
2023-06-09 13:46:35 -0700

    fixed app global styles, and login page styles.

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,73 @@
-import { Box, GlobalStyles, CssBaseline } from '@mui/material';
+import { Box, Stack, GlobalStyles, CssBaseline } from '@mui/material';
 import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
-import Header from './components/Header';
+import Header from './Header';
 import Sidebar from './Sidebar';
-import LoginField from './components/LoginField';
+import Login from './views/Login';
+import StewpotLogo from './components/svgComponents/StewpotLogo/StewpotLogo';
+import SvgGradient from './components/svgComponents/StewpotLogo/Gradient';
+import '../static/images/icons/logo/gradient.svg'
+// import { ReactComponent as StewPotLogo } from '../../static/images/icons/logo/stewpot-logo.svg'
+
+
+
 
 function App() {
-  const [ loggedIn, setLoggedIn ] = useState(true);
+  const [ loggedIn, setLoggedIn ] = useState(false);
+
+  const handleLoginAsGuest = () => {
+    setLoggedIn(true);
+  }
+
   return (
     <>
         <CssBaseline />
-        <GlobalStyles styles={{ html: {height: '100%'}, body: {height: '100%'}, '#root': {height: '100%'} }} />
+        <GlobalStyles styles={{ html: {height: '100%', overflow: 'hidden'}}} />
         {loggedIn ? (
-          <Box sx={{
-            display: 'flex'
-            
-          }}>
+          <Box sx={{display: 'flex'}}>
             <Sidebar />
-            <Box sx={{ height: '100%', display: 'flex', flexDirection: 'column', flexGrow: 1}}>
+            <Box sx={{ maxHeight: '100%', display: 'flex', flexDirection: 'column', flexGrow: 1}}>
               <Header />
               <Outlet />
-            </Box>
-          </Box> 
+            </Box> 
+          </Box>
           ) : (
-          <>
-            <LoginField inputLabel=' Username' inputType='username'></LoginField>
-            <LoginField inputLabel=' Password' inputType='password'></LoginField>
-          </>
+
+            <Box
+              height='100vh'
+              display='flex'
+              flexGrow={1}
+              justifyContent='center'
+              alignItems='center'
+              // sx={{
+              //   backgroundImage: 'url(../static/images/icons/logo/gradient.svg)',
+              //   backgroundSize: '100%',
+              //   backgroundPositionY: '100'
+              // }}
+            > 
+
+              <Box 
+                  position='fixed'
+                  zIndex={1}
+                  top={0}
+                  left={0}
+                  right={0}
+                  bottom={0}
+                  height='100vh'
+                  display='flex'
+                  flexGrow={1}
+                  justifyContent='center'
+                  alignItems='center'
+                  sx={{
+                    backgroundImage: 'url(../static/images/icons/logo/gradient.svg)',
+                    backgroundSize: '100%',
+                    backgroundPositionY: '100'
+                  }}
+                >
+                </Box>
+                <StewpotLogo color='#ff8b85' style={{height: 911, width: 911}} />
+                <Login handleLoginAsGuest={handleLoginAsGuest}/>
+            </Box>
           )
         }
     </>
