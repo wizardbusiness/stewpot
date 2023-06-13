899f3e19705c536e833faf7d9b1cd1306fda349d
Gabriel <gkime91@gmail.com>
2023-06-13 15:42:04 -0700

    updated parameter types on handleDragEnd

--- src/utils/sortUtils.ts
+++ src/utils/sortUtils.ts
@@ -1,13 +1,15 @@
 import { arrayMove} from "@dnd-kit/sortable";
 import { favoritesInterface } from "../consts/dummyData";
+import { Dispatch, SetStateAction } from "react";
 
-export const handleDragEnd = (e, setItems, items) => {
+
+export const handleDragEnd = (e, setItems: Dispatch<SetStateAction<favoritesInterface[]>>, items: favoritesInterface[]) => {
   const { active, over } = e;
   if (active.id !== over.id) {
     setItems((prevItems: favoritesInterface[]) => {
       const oldIndex = prevItems.findIndex((item) => item.id === active.id);
       const newIndex = prevItems.findIndex(item => item.id === over.id);
       return arrayMove(items, oldIndex, newIndex);
-    })
+    });
   }
 }
