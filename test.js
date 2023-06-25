81ec27a969c0503e6fa13426f32e7fd1665b3b94
Gabriel <gkime91@gmail.com>
2023-06-25 12:29:00 -0700

    removed package lock and test, unnecessary files.

--- test.js
+++
@@ -1,7 +0,0 @@
-const testArr = ['a', 'b', 'c']
-
-const testState = ['a', 'b', 'c', 'd', 'e'];
-
-const filteredState = testState.filter(item => testArr.includes(item));
-
-console.log(filteredState)
