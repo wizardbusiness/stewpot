a4ae079fb91f3417910f2b59a1d7d87bce4906d2
Gabriel <gkime91@gmail.com>
2023-06-25 12:00:40 -0700

    cleaned up unnecessary useeffects, connected to redux slice

--- src/views/Pantry.tsx
+++ src/views/Pantry.tsx
@@ -1,135 +1,74 @@
 import {useState, useEffect, useCallback} from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Outlet } from 'react-router-dom';
 import { Box, TextField, Button, Typography, styled} from '@mui/material';
 import Tooltip, {tooltipClasses, TooltipProps} from '@mui/material/Tooltip';
-import { DataGrid, GridColDef, GridRowsProp, GridPreProcessEditCellProps, GridRenderEditCellParams, GridEditInputCell} from '@mui/x-data-grid';
-import { yellow } from '@mui/material/colors';
+import { DeleteOutline } from '@mui/icons-material';
+import { GridRowsProp, GridColumnHeaderParams} from '@mui/x-data-grid';
 import SearchBar from '../components/common/SearchBar';
-import { pantryRows, pantryColumns } from '../consts/dummyData';
+import IngredientDataGrid from '../components/common/IngredientDataGrid';
+import { newRow } from '../consts/interfaces/pantry';
+import { RootState, AppDispatch } from '../consts/interfaces/redux';
 
-interface newRow {
-  id: number,
-  name: string,
-  qt: string,
-  unit: string,
-  location: string
-}
-
-const StyledDataGrid = styled(DataGrid)(({theme}) => ({
-  '& .super-row-theme--new-rows': {
-    backgroundColor: yellow[100],
-    '&:hover': {
-      backgroundColor: yellow[100],
-    },
-    '&.Mui-selected': {
-      backgroundColor: yellow[200],
-      '&:hover': {
-        backgroundColor: yellow[200]
-     }
-    },
-  },
-}));
-
-const StyledRejectionTooltip = styled(({className, ...props }: TooltipProps) => (
-  <Tooltip {...props} classes={{popper: className}} /> 
-))(({theme}) => ({
-  [`& .${tooltipClasses.tooltip}`]: {
-    backgroundColor: theme.palette.error.main,
-    color: theme.palette.error.contrastText
-  },
-}));
-
-
-const IngredientEditInputCell = (props: GridRenderEditCellParams) => {
-  const { error } = props;
-  return (
-    <StyledRejectionTooltip open={!!error} title={error}>
-        <GridEditInputCell {...props} />
-    </StyledRejectionTooltip>
-  )
-}
-
-const renderEditIngredient = (params: GridRenderEditCellParams) => {
-  return (<IngredientEditInputCell {...params} />)
-}
+// ingredient data for datagrid
 
 
 export default function Pantry() {
-  // saved ingredients
-  const [ allPantryColumns, setAllPantryColumns ] = useState<GridColDef[]>([]);
-  const [ allPantryRows, setAllPantryRows ] = useState<GridRowsProp>([]);
-  const [ searchedPantryRows, setSearchedPantryRows ] = useState<GridRowsProp>([]);
-  const [ oldRowsLength, setOldRowsLength ] = useState<number>(0);
 
-  const addNewRow = () => {
-    const updatedRows = [...allPantryRows];
+  const selectColumns = (state: RootState) => state.pantry.ingredientColumns;
+  const selectRows = (state: RootState) => state.pantry.ingredientRows;
+
+  const rows = useSelector(selectRows);
+  const columns = useSelector(selectColumns);
+
+  const [ searchedRows, setSearchedRows ] = useState(rows);
+
+  const dispatch = useDispatch();
+
+
+  const handleAddNewRow = () => {
 
     const newRow: newRow = {
-      id: allPantryRows.length + 1,
+      id: rows.length + 1,
       name: 'New Ingredient',
       qt: '0',
       unit: '0',
       location: 'Set Location'
-    };
-    updatedRows.unshift(newRow);
-    setAllPantryRows(updatedRows);
-    setSearchedPantryRows(updatedRows);
-  };
+    }
+    dispatch({type: 'pantry/addRow', payload: newRow})
+    setSearchedRows([newRow, ...searchedRows])
+  }
 
-  const removeRow = useCallback((id: number) => {
-    const updatedRows = [...allPantryRows].filter((row) => row.id !== id);
-    setAllPantryRows(updatedRows);
-    setSearchedPantryRows(updatedRows)
-  }, [allPantryRows]);
+  const handleDeleteRow = (e, id: number) => {
+    dispatch({type: 'pantry/removeRow', payload: id});
+    setSearchedRows(searchedRows.filter(row => row.id !== id));
+  }
 
-  const preProcessEditCellProps = useCallback((params: GridPreProcessEditCellProps) => {
-    const validateRow = (ing: string) => {
-      const ingredients = allPantryRows.map(row => row.ingredient.toLowerCase());
-      const exists = ingredients.includes(ing.toLowerCase());
-      return (exists ? `${ing} in pantry` : null);
-    }; 
-    
-    const errorMessage = validateRow(params.props.value.toString());
-    return { ...params.props, error: errorMessage}
-  }, [allPantryRows])
-
-  useEffect(() => {
-    setAllPantryRows(pantryRows);
-    setSearchedPantryRows(pantryRows);
-    setOldRowsLength(pantryRows.length);
-  }, [])
-
-  useEffect(() => {
-    const updatedPantryColumns: GridColDef[] = [...pantryColumns]
-    updatedPantryColumns.push({
-      field: 'removeRow', 
-      headerName: 'Remove Ingredient', 
-      width: 150,
-      renderCell: (params) => <Button onClick={() => removeRow(params.row.id)}>Delete</Button>
-    });
-    
-    const procUpdatedPantryColumns = updatedPantryColumns.map(col => {
-      return {
-        ...col, 
-        preProcessEditCellProps,
-        renderEditCell: renderEditIngredient
-      }
-    }); 
-
-    setAllPantryColumns(procUpdatedPantryColumns);
-  }, [allPantryRows, preProcessEditCellProps, removeRow])
+  const pantryColumns = [...columns, {
+    headerAlign: 'center',
+    field: '',
+    renderHeader: (params: GridColumnHeaderParams) => <DeleteOutline />,
+    renderCell: (cellParams) => {
+      return (
+        <Button
+          variant='outlined'
+          onClick={(e) => {
+            handleDeleteRow(e, cellParams.id);
+          }}
+        >
+          Delete
+        </Button>
+      )
+    }
+  }];
 
   return (  
-    <Box sx={{padding: '2em', margin: '1em'}}>
+    <>
       <Typography flexShrink={1} color='#616161' variant='h2'>Pantry</Typography>
-        <SearchBar placeholderText='Search Ingredients' data={allPantryRows} dataProperty='name' setResults={setSearchedPantryRows} />
-        <Button variant='contained' onClick={addNewRow}>+ Add New Ingredient</Button>
-        <StyledDataGrid 
-          getRowClassName={(params) => params.row.id > oldRowsLength ? `super-row-theme--new-rows` : `super-row-theme${params.row.id}` } 
-          rows={searchedPantryRows} 
-          columns={allPantryColumns} 
-        />
+      <SearchBar placeholderText='Search Ingredients' data={rows} dataProperty='name' setSearchResults={setSearchedRows} />
+      <Button variant='contained' onClick={handleAddNewRow}>+ Add New Ingredient</Button>
+      <IngredientDataGrid rows={searchedRows} columns={pantryColumns}/>
       <Outlet />
-    </Box>
+    </>
   )
 }
