6784da2303e058383ed05d02d909d906b8cddebc
Gabriel <gkime91@gmail.com>
2023-06-26 18:05:17 -0700

    i can remove this.

--- src/components/svgComponents/logoAnimation.ts
+++ src/components/svgComponents/logoAnimation.ts
@@ -1,5 +1,5 @@
 
-const animateBubbles = async (bubbleQueue) {
+const animateBubbles = async (bubbleQueue) => {
   // animate each bubble upwards along y axis
   // time emergence of bubbles so that once the bubble travels specified distance, it is added back to the queue
   bubbleQueue.forEach(bubble => {
