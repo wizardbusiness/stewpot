559c9ac2bb0804c1637912ce99c326abb89c5d90
Gabriel <gkime91@gmail.com>
2023-07-03 12:13:26 -0700

    tried following strat for integrating express server but failed apparently due to typescript. will create new branch to work on.

--- server/server.ts
+++ server/server.ts
@@ -12,59 +12,82 @@ import userRoutes from './routes/userRoutes';
 import spoonacularRoutes from './routes/spoonacularAPI';
 import {ServerError} from './types';
 
+const isTest = process.env.NODE_ENV === 'test';
 
-const createServer = async () => {
+const createServer = async (root = process.cwd(), isProd = process.env.NODE_ENV === 'production') => {
+
+  const resolve = (p: string) => path.resolve(__dirname, p);
+
   const app = express();
-  const resolve = (p) => path.resolve(__dirname, p)
 
-  const vite = await createViteServer({
-    server: { middlewareMode: true},
-    appType: 'custom',
-  })
+  let vite; 
+  
+  if (isProd) {
+    app.use(express.static(resolve('../dist')))
+  } else {
+    vite = await createViteServer({
+      root,
+      logLevel: isTest ? 'error' : 'info',
+      server: { 
+        middlewareMode: true,
+        watch: {
+          usePolling: true,
+          interval: 100
+        }
+      }
+    });
+  };
+  
+  
 
-app.use(vite.middlewares);
+  app.use(vite.middlewares);
 
-// serve static assets
-app.use(express.static('./assets'));
+  app.use('*', async (req, res) => {
+    const url = req.originalUrl;
 
-// body parser for static files.
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+      const template = fs.readFileSync(
+        resolve(isProd ? '../dist/main.js' : './index.html'),
+        'utf-8'
+      );
+    
+    res.status(200).set({ 'Content-Type': 'text/html' }).end(template);
+  });
 
-// db routes
-app.use('/api', pantryRoutes);
-app.use('/api', favRoutes);
-app.use('/api/user', userRoutes);
-// spoonacular routes
-app.use('/api', spoonacularRoutes);
+  // serve static assets
+  // app.use(express.static('./assets'));
 
-app.use('*', async (req, res) => {
-  const url = req.originalUrl;
+  // // body parser for static files.
+  // app.use(express.json());
+  // app.use(express.urlencoded({extended: true}));
 
-  const template = fs.readFileSync(
-    resolve('./index.html'),
-    'utf-8'
+  // // db routes
+  // app.use('/api', pantryRoutes);
+  // app.use('/api', favRoutes);
+  // app.use('/api/user', userRoutes);
+  // // spoonacular routes
+  // app.use('/api', spoonacularRoutes);
+
+  
+
+  app.use(
+    '/',
+    (err, req, res, next) => {
+      const defaultErr = {
+        log: 'Express error handler caught unknown middleware error',
+        status: 400,
+        message: {err: 'An error occurred'},
+      };
+      const errorObj = Object.assign({}, defaultErr, err);
+      console.log(errorObj.log);
+      return res.status(errorObj.status).json(errorObj.message);
+    }
   );
 
-  res.status(200).set({ 'Content-Type': 'text/html' }).end(template);
-});
-
-app.use(
-  '/',
-  (err, req, res, next) => {
-    const defaultErr = {
-      log: 'Express error handler caught unknown middleware error',
-      status: 400,
-      message: {err: 'An error occurred'},
-    };
-    const errorObj = Object.assign({}, defaultErr, err);
-    console.log(errorObj.log);
-    return res.status(errorObj.status).json(errorObj.message);
-  }
-);
-
-return { app, vite } 
-
+  return { app, vite } 
 }
 
-createServer().then(({ app }) => app.listen(3000, () => console.log('server is listening on port 3000')));
+if (!isTest) {
+  createServer().then(({ app }) => app.listen(3000, () => console.log('server is listening on port 3000')));
+}
+
+export default createServer;
