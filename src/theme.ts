6cc47841c9d52fa9239e438e1a9c476b22db0b69
Gabriel <gkime91@gmail.com>
2023-07-15 19:42:39 -0700

    added theme file (unused for now).

---
+++ src/theme.ts
@@ -0,0 +1,15 @@
+import { createTheme, Theme } from '@mui/material/styles';
+
+const theme: Theme = createTheme({
+  palette: {
+    primary: {
+      main: '#EFEFEF'
+    },
+    secondary: {
+      main: '#FAAA63'
+    }
+  },
+  components: {
+    
+  }
+})
