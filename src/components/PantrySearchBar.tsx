16f8e317b72d6e886abbed9894f6964ad3eb57d9
Gabriel <gkime91@gmail.com>
2023-07-15 20:02:19 -0700

    simplified search bar, removed search button, reverted to realtime search while typing.

--- src/components/PantrySearchBar.tsx
+++ src/components/PantrySearchBar.tsx
@@ -1,5 +1,5 @@
-import { useState, SyntheticEvent, Dispatch, SetStateAction } from 'react';
-import { Box, TextField, Button,  } from '@mui/material';
+import React, { SyntheticEvent, Dispatch, SetStateAction } from 'react';
+import { Box, TextField, } from '@mui/material';
 import { GridRowsProp } from '@mui/x-data-grid';
 
 interface PantrySearchBarProps {
@@ -10,22 +10,16 @@ interface PantrySearchBarProps {
 
 const PantrySearchBar = ({placeholderText, ingredients, setResults} : PantrySearchBarProps) => {
 
-  const [ searchedStr, setSearchedStr ] = useState<string> ('');
-  const trackerUserInput = (e: SyntheticEvent): void => {
-    e.preventDefault();
-    const target = e.target as HTMLInputElement;
-    const searchStr = target.value.toLowerCase().trim();
-    setSearchedStr(searchStr);
-  };
-
   // only searches by title right now, would like to be able to search by other properties.
   const handleSearchByProperty = (e: SyntheticEvent): void => {
     e.preventDefault();
+    const target = e.target as HTMLInputElement;
+    const searchStr = target.value.toLowerCase().trim();
     const searchRes = ingredients.filter((ingredient)  => {
       const property = ingredient.name.toString().toLowerCase();
       const wordsInProperty = ingredient.name.toString().toLowerCase().split(' ');
       const allValidSearchCombs = new Set([...wordsInProperty, property]);    
-      const searchedWords = [...allValidSearchCombs].filter((word: string) => word.startsWith(searchedStr));
+      const searchedWords = [...allValidSearchCombs].filter((word: string) => word.startsWith(searchStr));
       return property.includes(searchedWords[0]);
     });
     setResults(searchRes);
@@ -33,18 +27,15 @@ const PantrySearchBar = ({placeholderText, ingredients, setResults} : PantrySear
   };
 
   return (
-    <form onSubmit={handleSearchByProperty}>
       <Box display='flex' alignItems='center' padding='2em 0 2em 0'>
         <TextField 
-          onChange={trackerUserInput} 
+          onChange={handleSearchByProperty} 
           placeholder={placeholderText}
           variant='outlined' 
           size='medium'  
           sx={{width: '29%'}}
         />
-        <Button type='submit' variant='contained' sx={{padding: '1em'}}size='large'>Go</Button>
       </Box>
-    </form>
   );
 };
 
