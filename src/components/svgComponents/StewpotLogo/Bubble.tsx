65077d999eea97631c7df61ee97731294dea53b9
Gabriel <gkime91@gmail.com>
2023-10-09 18:05:06 -0700

    fixed react imports

--- src/components/svgComponents/StewpotLogo/Bubble.tsx
+++ src/components/svgComponents/StewpotLogo/Bubble.tsx
@@ -1,22 +1,25 @@
-import {useState, useEffect} from 'react'
-import PoppedBubble from './PoppedBubble';
-import UnpoppedBubble from './UnpoppedBubble';
-import { BubbleProps } from '../../../consts/interfaces/componentInterfaces';
+import React, { useState, useEffect } from "react";
+import PoppedBubble from "./PoppedBubble";
+import UnpoppedBubble from "./UnpoppedBubble";
+import { BubbleProps } from "../../../consts/interfaces/componentInterfaces";
 
-
-
-const Bubble = ({animDur, animDela, offset, classNmPop, classNmUnPop }: BubbleProps) => {
-
-  const [ bubble, setBubble ] = useState<boolean>(true);
-  const [ popped, setPopped ] = useState<boolean>(false);
-  const [ cycleEnd, setCycleEnd ] = useState<boolean>(false);
-  const [ hideBubble, setHideBubble ] = useState<boolean>(true);
+const Bubble = ({
+  animDur,
+  animDela,
+  offset,
+  classNmPop,
+  classNmUnPop,
+}: BubbleProps) => {
+  const [bubble, setBubble] = useState<boolean>(true);
+  const [popped, setPopped] = useState<boolean>(false);
+  const [cycleEnd, setCycleEnd] = useState<boolean>(false);
+  const [hideBubble, setHideBubble] = useState<boolean>(true);
 
   useEffect(() => {
     const bubbleTimer = setTimeout(() => setHideBubble(false), animDela * 1000);
     return () => clearTimeout(bubbleTimer);
-  }, [animDela])
-  
+  }, [animDela]);
+
   const handlePopBubble = (): void => {
     setPopped(true);
     setBubble(false);
@@ -33,38 +36,38 @@ const Bubble = ({animDur, animDela, offset, classNmPop, classNmUnPop }: BubblePr
     return () => clearTimeout(bubbleTimer);
   }, [cycleEnd, hideBubble]);
 
-
   return (
     <>
-      {!hideBubble && <g>
-        {!cycleEnd &&
-          <UnpoppedBubble
-            bubble={bubble} 
-            animDur={animDur} 
-            animDela={animDela}
-            offset={offset}
-            classNmPop={classNmPop}
-            classNmUnPop={classNmUnPop}
-            handlePopBubble={handlePopBubble}
-            setBubble={setBubble}
-            setPopped={setPopped}
-            setCycleEnd={setCycleEnd}
-          />
-        }
-        {!cycleEnd && 
-          <PoppedBubble 
-            popped={popped}
-            offset={offset}
-            animDur={animDur}
-            animDela={animDela}
-            classNmPop={classNmPop}
-            classNmUnPop={classNmUnPop}
-          />
-        }
-      </g>
-    }
+      {!hideBubble && (
+        <g>
+          {!cycleEnd && (
+            <UnpoppedBubble
+              bubble={bubble}
+              animDur={animDur}
+              animDela={animDela}
+              offset={offset}
+              classNmPop={classNmPop}
+              classNmUnPop={classNmUnPop}
+              handlePopBubble={handlePopBubble}
+              setBubble={setBubble}
+              setPopped={setPopped}
+              setCycleEnd={setCycleEnd}
+            />
+          )}
+          {!cycleEnd && (
+            <PoppedBubble
+              popped={popped}
+              offset={offset}
+              animDur={animDur}
+              animDela={animDela}
+              classNmPop={classNmPop}
+              classNmUnPop={classNmUnPop}
+            />
+          )}
+        </g>
+      )}
     </>
-  )
-}
+  );
+};
 
 export default Bubble;
