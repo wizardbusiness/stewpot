fbe04b225ecc43448372d03ee33c14ba2f490853
Gabriel <gkime91@gmail.com>
2024-01-10 18:38:12 -0800

    linting

--- server/routes/pantryRoutes.ts
+++ server/routes/pantryRoutes.ts
@@ -4,15 +4,18 @@ import pantryController from '../controllers/pantryController';
  
 const pantryRouter = express.Router();
 
-pantryRouter.route('/pantry')
+pantryRouter.route('/pantry/rows')
   .get(pantryController.getAllPantryItems, (req: Request, res: Response, next: NextFunction) => {
     res.json(res.locals.pantry)
-}).post(pantryController.addPantryItem, (req: Request, res: Response, next: NextFunction) => {
-    res.json(res.locals.pantry)
-}).put(pantryController.editPantryItem, (req: Request, res: Response, next: NextFunction) => {
-    res.json(res.locals.pantry)
-}).delete(pantryController.deletePantryItem, (req: Request, res: Response, next: NextFunction) => {
-    res.json(res.locals.pantry)
-});
+  })
+  .post(pantryController.addPantryItem, (req: Request, res: Response, next: NextFunction) => {
+      res.json(res.locals.pantry)
+  })
+  .put(pantryController.editPantryItem, (req: Request, res: Response, next: NextFunction) => {
+      res.json(res.locals.pantry)
+  })
+  .delete(pantryController.deletePantryItem, (req: Request, res: Response, next: NextFunction) => {
+      res.json(res.locals.pantry)
+  });
 
 export default pantryRouter;
