b7e778aff9770b27a29eb55c806a097b8f7bb6fe
Gabriel <gkime91@gmail.com>
2024-01-10 17:15:21 -0800

    finished setting up store boilerplate

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import pantryReducer from "./slices/pantrySlice";
-import { TypedUseSelectorHook, useDispatch, useSelector  } from "react-redux";
 
 const store = configureStore({
   reducer: {
-    pantry: pantryReducer
-  }
+    pantry: pantryReducer,
+  },
 });
 
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
 export default store;
