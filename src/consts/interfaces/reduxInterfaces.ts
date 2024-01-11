58dc470ae399a4334b1ea3fb73f7d14858fb1e3e
Gabriel <gkime91@gmail.com>
2024-01-10 17:16:03 -0800

    added autocomplete to pantry reducer state interface

--- src/consts/interfaces/reduxInterfaces.ts
+++ src/consts/interfaces/reduxInterfaces.ts
@@ -1,5 +1,12 @@
 import store from "../../redux/store";
+import { GridRowsProp } from "@mui/x-data-grid";
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
+export interface PantryState {
+  isLoading: boolean;
+  ingredientRows: GridRowsProp;
+  autocompleteData: string[];
+  commonIngredients: string[];
+}
